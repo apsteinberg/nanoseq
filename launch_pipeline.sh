2a893721f641ec875e23218d47a70d5750b1faf7
apsteinberg <asherps@gmail.com>
2024-05-21 17:29:54 -0400

    MSK commits

--- launch_pipeline.sh
+++ launch_pipeline.sh
@@ -18,7 +18,7 @@ module load singularity/3.7.1
 module load java/20.0.1
 ## example samplesheet
 ## technical replicates get merged ...
-samplesheet=${HOME}/nanoseq/resources/samplesheet.csv
+samplesheet=${HOME}/nanoseq/resources/test_samplesheet.csv
 ## specify path to out directory
 outdir=/data1/shahs3/users/preskaa/APS022_Archive/240516_nanoseq_test
 
